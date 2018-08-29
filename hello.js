a62caf339d5656a94dc6da0f22c171e86a66ef6e
vandana1499 <vandanagupta1499@gmail.com>
2018-08-30 03:48:47 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,10 @@
+/*alert("hello js");
+document.write("<h1>this is javascritopt</h1>");
+alert("thank yoy");*/
+var first=prompt("how is the programmer??");
+var sec=prompt("whw he does??");
+var third=prompt("how is she??");
+var sentence="<h1>the programmer is "+first;
+sentence+=".She does "+sec;
+sentence+=".She is "+third;
+document.write(sentence);
